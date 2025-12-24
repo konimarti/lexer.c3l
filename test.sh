918458685082cf03f0965e526f8f5b5490191586
Koni Marti <koni.marti@gmail.com>
2025-12-24 09:45:30 +0100

    lexer: rename DocLexer to ContractLexter, cleanup, test only stdlib and unit tests

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ c3c build lexv
 
 files=()
 files+=( $(find ../c3c/lib -name "*.c3") )
-files+=( $(find ../c3c/test/ -name "*.c3" ! -name expected_directive.c3 ! -name parse_hex_float.c3 ! -name bad_bitwidth.c3) )
+files+=( $(find ../c3c/test/unit -name "*.c3" ! -name expected_directive.c3 ! -name parse_hex_float.c3 ! -name bad_bitwidth.c3) )
 c3c -E compile-only ${files[@]} | build/lexv
